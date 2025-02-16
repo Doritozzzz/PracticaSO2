b066a7498d778bf7d528b063a5208c4637457bc4
Doritoz <javier.vivo.samaniego@gmail.com>
2025-02-16 17:59:56 +0100

    tamMB()

--- bloques.c
+++ bloques.c
@@ -16,6 +16,7 @@ int bmount(const char *camino) {
 
     // Abrir el fichero en modo lectura y escritura
     descriptor = open(camino, O_RDWR | O_CREAT, 0666);
+    
     // Comprobar si se ha abierto correctamente, devuelve FALLO en caso contrario
     if (descriptor == -1) {
         fprintf(stderr, "Error en la apertura del fichero %s: %s\n", camino, strerror(errno));
--- ficheros_basicos.c
+++ ficheros_basicos.c
@@ -1,1 +1,27 @@
-#include "ficheros_basicos.h"
+#include "ficheros_basicos.h"\
+
+/**
+ * tamMB --> Función para calcular el tamaño del mapa de bits en bloques
+ * @param nbloques: Número de bloques del dispositivo
+ * @return Tamaño del mapa de bits en bloques
+ */
+int tamMB(unsigned int nbloques){
+    // Calculamos el tamaño del mapa de bits en bloques
+    int tamMB = (nbloques / 8) / BLOCKSIZE;
+
+    // Si el tamaño del mapa de bits en bloques no es exacto, añadimos un bloque más
+    if (nbloques % (8 * BLOCKSIZE) != 0) {
+        tamMB++;
+    }
+
+    // Devolvemos el tamaño del mapa de bits en bloques
+    return tamMB;
+}
+
+int tamAI(unsigned int ninodos);
+
+int initSB(unsigned int nbloques, unsigned int ninodos);
+
+int initMB();
+
+int initAI();
