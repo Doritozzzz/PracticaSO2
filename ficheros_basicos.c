b4a5139454ff6319d15460f6045cef7b7d16b93f
Doritoz <javier.vivo.samaniego@gmail.com>
2025-02-16 18:48:59 +0100

    initSB

--- ficheros_basicos.c
+++ ficheros_basicos.c
@@ -1,16 +1,18 @@
-#include "ficheros_basicos.h"\
+#include "ficheros_basicos.h"
 
 /**
  * tamMB --> Función para calcular el tamaño del mapa de bits en bloques
  * @param nbloques: Número de bloques del dispositivo
  * @return Tamaño del mapa de bits en bloques
  */
-int tamMB(unsigned int nbloques){
+int tamMB(unsigned int nbloques)
+{
     // Calculamos el tamaño del mapa de bits en bloques
     int tamMB = (nbloques / 8) / BLOCKSIZE;
 
     // Si el tamaño del mapa de bits en bloques no es exacto, añadimos un bloque más
-    if (nbloques % (8 * BLOCKSIZE) != 0) {
+    if (nbloques % (8 * BLOCKSIZE) != 0)
+    {
         tamMB++;
     }
 
@@ -23,20 +25,59 @@ int tamMB(unsigned int nbloques){
  * @param ninodos: Número de inodos del dispositivo (ninodos=nbloques/4)
  * @return Tamaño del array de inodos en bloques
  */
-int tamAI(unsigned int ninodos){
+int tamAI(unsigned int ninodos)
+{
     // Calculamos el tamaño del array de inodos en bloques
     int tamAI = (ninodos * INODOSIZE) / BLOCKSIZE;
 
     // Si el tamaño del array de inodos en bloques no es exacto, añadimos un bloque más (como en tamMB)
-    if ((ninodos * INODOSIZE) % BLOCKSIZE != 0) {
+    if ((ninodos * INODOSIZE) % BLOCKSIZE != 0)
+    {
         tamAI++;
     }
 
     // Devolvemos el tamaño del array de inodos en bloques
     return tamAI;
 }
+/**
+ * initSB --> Función para inicializar el superbloque
+ * @param nbloques: Número de bloques del dispositivo
+ * @param ninodos: Número de inodos del dispositivo (ninodos=nbloques/4)
+ * @return EXITO si se ha inicializado correctamente, FALLO en caso contrario
+ */
+int initSB(unsigned int nbloques, unsigned int ninodos) {
+    // COmprobamos que nbloques y ninodos no sean cero
+    if (nbloques == 0 || ninodos == 0) {
+        fprintf(stderr, "Error: nbloques o ninodos no pueden ser cero.\n");
+        return FALLO;
+    }
 
-int initSB(unsigned int nbloques, unsigned int ninodos);
+    // Inicializamos el superbloque
+    struct superbloque SB;
+    SB.posPrimerBloqueMB = posSB + tamSB;
+    SB.posUltimoBloqueMB = SB.posPrimerBloqueMB + tamMB(nbloques) - 1;
+    SB.posPrimerBloqueAI = SB.posUltimoBloqueMB + 1;
+    SB.posUltimoBloqueAI = SB.posPrimerBloqueAI + tamAI(ninodos) - 1;
+    SB.posPrimerBloqueDatos = SB.posUltimoBloqueAI + 1;
+    SB.posUltimoBloqueDatos = nbloques - 1;
+    SB.posInodoRaiz = 0;
+    SB.posPrimerInodoLibre = 0;
+    SB.cantBloquesLibres = nbloques;
+    SB.cantInodosLibres = ninodos;
+    SB.totBloques = nbloques;
+    SB.totInodos = ninodos;
+
+    // Escribimos el superbloque en el dispositivo virtual
+    int resultado = bwrite(posSB, &SB);
+    if (resultado != EXITO) {
+        fprintf(stderr, "Error en la escritura del SB en el bloque %d: %d\n", posSB, resultado);
+        return FALLO;
+    }
+
+    // Devolvemos EXITO si se ha inicializado correctamente
+    return EXITO;
+}
+
 
 int initMB();
 
